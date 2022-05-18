612953ca1e63449982e9e98b711e25aca4ca5ddb
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-18 22:54:26 +0500

    mobile fix

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// $(document).ready(function () {
-//   $("#btn-login").click(function () {
-//     $("#background").show();
-//     $("#popup-wrapper").show();
-//   });
-//   $("#cross").click(function () {
-//     $("#background").hide();
-//     $("#popup-wrapper").hide();
-//   });
-//   $("#background").click(function () {
-//     $(this).hide();
+$(document).ready(function () {
+  // popup-1
+  $("#btn-1-next").click(function () {
+    $("#popup-2").show();
+    $("#popup-1").hide();
+  });
 
-//     $("#popup-wrapper").hide();
-//   });
-// });
+  $("#btn-2-prev").click(function () {
+    $("#popup-1").show();
+    $("#popup-2").hide();
+  });
+
+  $("#btn-2-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").show();
+  });
+
+  $("#btn-3-prev").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").show();
+    $("#popup-3").hide();
+  });
+
+  $("#btn-3-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").show();
+    $("#popup-3").hide();
+  });
+
+  $("#option-1").click(function () {
+    $(this).toggleClass("option-selected");
+    $("#option-A").toggleClass("option");
+  });
+
+  $("#option-2").click(function () {
+    $(this).toggleClass("option-selected");
+    $("#option-B").toggleClass("option");
+  });
+
+  $("#option-3").click(function () {
+    $(this).toggleClass("option-selected");
+    $("#option-C").toggleClass("option");
+  });
+
+  $("#option-4").click(function () {
+    $(this).toggleClass("option-selected");
+    $("#option-D").toggleClass("option");
+  });
+});
