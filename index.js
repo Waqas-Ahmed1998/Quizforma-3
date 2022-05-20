f8eb067021f8454adf27c76c95c0e860d27759e9
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-20 21:20:56 +0500

    background color fix

--- index.js
+++ index.js
@@ -31,20 +31,38 @@ $(document).ready(function () {
   $("#option-1").click(function () {
     $(this).toggleClass("option-selected");
     $("#option-A").toggleClass("option");
+
+    var option1Input = $("#option-1-input");
+    option1Input.prop("checked", !option1Input.prop("checked"));
+    console.log(option1Input.is(":checked"));
   });
 
   $("#option-2").click(function () {
     $(this).toggleClass("option-selected");
     $("#option-B").toggleClass("option");
+
+    var option2Input = $("#option-2-input");
+    option2Input.prop("checked", !option2Input.prop("checked"));
+    console.log(option2Input.is(":checked"));
   });
 
   $("#option-3").click(function () {
     $(this).toggleClass("option-selected");
     $("#option-C").toggleClass("option");
+
+    var option3Input = $("#option-3-input");
+    option3Input.prop("checked", !option3Input.prop("checked"));
+    console.log(option3Input.is(":checked"));
   });
 
   $("#option-4").click(function () {
     $(this).toggleClass("option-selected");
     $("#option-D").toggleClass("option");
+
+    var option4Input = $("#option-4-input");
+    option4Input.prop("checked", !option4Input.prop("checked"));
+    console.log(option4Input.is(":checked"));
   });
+
+  // input check
 });
